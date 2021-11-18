5f81eabafe19e1e998f9a9e08e37a33d37fe8a22
phastandco <hugo@Mac-Cec.local>
2021-11-18 10:11:39 +0100

    add city modelé

--- login/admin.py
+++ login/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Contact
+from .models import Contact, Ville
 
 admin.site.register(Contact)
+admin.site.register(Ville)
--- login/models.py
+++ login/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 # Null = True => champ obligatoire
 
@@ -12,5 +13,23 @@ class Contact(models.Model):
     mobile = models.CharField(max_length = 12, blank = True, null = True)
     date_added = models.DateTimeField(auto_now_add = True)
 
-    def __str__ (self):
+    def __str__(self):
         return self.name
+
+
+class Ville(models.Model):
+    name = models.CharField(max_length = 100, null = True)
+    location = models.ImageField\
+        (upload_to = "static/images/locations",
+         height_field = 100,
+         width_field = 200,
+         max_length = 100,
+         blank = True,
+         null = True)
+    population = models.PositiveIntegerField()
+    contact = Contact()
+    date_added = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.name
+    
